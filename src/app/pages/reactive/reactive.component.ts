c9d4c386a75d0b6eabfb6d3711054e6866486272
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-11-12 00:55:48 -0600

    video 205

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -50,9 +50,7 @@ export class ReactiveComponent implements OnInit {
         ciudad  : ['',Validators.required],
 
       }),
-      pasatiempos: this.fb.array([
-        [],[],[],[],[]
-      ])
+      pasatiempos: this.fb.array([])
     });
   }
 
@@ -66,11 +64,19 @@ export class ReactiveComponent implements OnInit {
       direccion: {
         distrito:'Tlaxcala',
         ciudad: 'Apizaco'
-      }
+      },
     });
 
   }
 
+
+
+  agregarPasatiempo(){
+    this.pasatiempos.push( this.fb.control('') );
+  }
+  borrarPasatiempo(i: number){
+    this.pasatiempos.removeAt(i);
+  }
 
 
   guardar(){
