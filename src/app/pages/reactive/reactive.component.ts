68463abe3fdc2bd4a4fb817973fe567b7e7ae0e2
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-11-11 19:23:29 -0600

    video 198

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+  this.crearFormulario();
+    this.forma = this.fb.group({
+      nombre  : ['Emanuel'],
+      apellido: ['Rangel'],
+      correo  : ['emanuel@gmail.com'],
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  crearFormulario(){
+
+  }
+
+
+  guardar(){
+    console.log(this.forma);
+    
   }
 
 }
